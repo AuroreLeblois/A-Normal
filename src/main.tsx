bda9963ccef000db19e9e7b85dd4ca1104eef49a
Aurore Leblois <51274472+AuroreLeblois@users.noreply.github.com>
2026-01-31 14:41:06 +0100

    revert changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,6 @@ import { ThemeProvider } from 'react-kariu'
 import App from './App'
 import './App.css'
 
-// Set favicon with correct base path
-const favicon = document.getElementById('favicon') as HTMLLinkElement
-if (favicon) {
-  favicon.href = `${import.meta.env.BASE_URL}dice.svg`
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider initialTheme="dark">
