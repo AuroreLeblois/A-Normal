0fadfcb664870e75401016f85b445538e7f529e6
Aurore Leblois <leblois-aurore@hotmail.fr>
2026-04-24 17:03:43 +0200

    up type

--- src/i18n/fr.ts
+++ src/i18n/fr.ts
@@ -253,4 +253,16 @@ export const fr = {
   },
 } as const
 
-export type TranslationSchema = typeof fr
+type WidenLiterals<T> = T extends string
+  ? string
+  : T extends number
+  ? number
+  : T extends boolean
+  ? boolean
+  : T extends readonly (infer U)[]
+  ? WidenLiterals<U>[]
+  : T extends object
+  ? { [K in keyof T]: WidenLiterals<T[K]> }
+  : T
+
+export type TranslationSchema = WidenLiterals<typeof fr>
